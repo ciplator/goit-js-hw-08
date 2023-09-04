3c54a47c3fef22124c3c911f7151c731cd122e74
Dmitry Sukhorukov <ciplator@gmail.com>
2023-09-04 22:30:18 +0300

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,29 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
-import SimpleLightbox from "simplelightbox"; // Импорт библиотеки SimpleLightbox
-import "simplelightbox/dist/simple-lightbox.min.css"; // Импорт CSS библиотеки
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
-const galleryList = document.querySelector(".gallery");
+const galleryContainer = document.querySelector(".gallery");
+const markup = galleryItems.reduce(
+  (acc, { original, preview, description }) =>
+    (acc += `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`),
+  ""
+);
 
-galleryList.innerHTML = galleryItems
-  .map(
-    ({ preview, original, description }) =>
-      `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img class="gallery__image" src="${preview}" alt="${description}" />
-        </a>
-      </li>`
-  )
-  .join("");
-galleryList.addEventListener("click", (event) => {
-  event.preventDefault();
+galleryContainer.insertAdjacentHTML("beforeend", markup);
 
-  if (event.target.classList.contains("gallery__image")) {
-    const largeImageUrl = event.target.parentNode.href;
-
-    const instance = basicLightbox.create(
-      `<img src="${largeImageUrl}" alt="Велике зображення">`
-    );
-
-    instance.show();
-  }
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
 });
